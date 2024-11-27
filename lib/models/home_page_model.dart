db07ba3e2c2f9a0142a751a0e4f592f8f3926cad
Umang Hirani <umanghirani.exe@gmail.com>
2024-11-28 00:53:26 +0530

    ticking the task complete + text overflow error solve

--- lib/models/home_page_model.dart
+++ lib/models/home_page_model.dart
@@ -11,49 +11,11 @@ class HomePageModel extends FlutterFlowModel<HomePageWidget> {
   int get tabBarCurrentIndex =>
       tabBarController != null ? tabBarController!.index : 0;
 
-  // Model for TaskTile component.
-  late TaskTileModel taskTileModel1;
-  // Model for TaskTile component.
-  late TaskTileModel taskTileModel2;
-  // Model for TaskTile component.
-  late TaskTileModel taskTileModel3;
-  // Model for TaskTile component.
-  late TaskTileModel taskTileModel4;
-  // Model for TaskTile component.
-  late TaskTileModel taskTileModel5;
-  // Model for TaskTile component.
-  late TaskTileModel taskTileModel6;
-  // Model for TaskTile component.
-  late TaskTileModel taskTileModel7;
-  // Model for TaskTile component.
-  late TaskTileModel taskTileModel8;
-  // Model for TaskTile component.
-  late TaskTileModel taskTileModel9;
-
   @override
-  void initState(BuildContext context) {
-    taskTileModel1 = createModel(context, () => TaskTileModel());
-    taskTileModel2 = createModel(context, () => TaskTileModel());
-    taskTileModel3 = createModel(context, () => TaskTileModel());
-    taskTileModel4 = createModel(context, () => TaskTileModel());
-    taskTileModel5 = createModel(context, () => TaskTileModel());
-    taskTileModel6 = createModel(context, () => TaskTileModel());
-    taskTileModel7 = createModel(context, () => TaskTileModel());
-    taskTileModel8 = createModel(context, () => TaskTileModel());
-    taskTileModel9 = createModel(context, () => TaskTileModel());
-  }
+  void initState(BuildContext context) {}
 
   @override
   void dispose() {
     tabBarController?.dispose();
-    taskTileModel1.dispose();
-    taskTileModel2.dispose();
-    taskTileModel3.dispose();
-    taskTileModel4.dispose();
-    taskTileModel5.dispose();
-    taskTileModel6.dispose();
-    taskTileModel7.dispose();
-    taskTileModel8.dispose();
-    taskTileModel9.dispose();
   }
 }
